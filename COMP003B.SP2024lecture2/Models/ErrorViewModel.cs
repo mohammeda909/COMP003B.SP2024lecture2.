06c37eb9789a81d616f6505921f98f1eb71af85f
mohammedalahmadi <mohammedalahmadi@LEM278-15>
2024-04-01 18:33:15 -0700

    Add project files.

---
+++ COMP003B.SP2024lecture2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace COMP003B.SP2024lecture2_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
